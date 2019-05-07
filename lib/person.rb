ea62fae31b2ed035962f385eac5fd629255a1285
Meg Galvez <meg@oakmaine.com>
2019-05-07 14:31:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,17 @@
-class Person 
-  def initialize(name)
-    @name = name
-  end 
-  
-  def name=(name)
-    @name = name
-  end 
-  
-  def name
-    @name
-  end 
+def math(array)
+  math_hash = {}
+    mean = 0
+    array.each do |a| 
+      mean+=a
+    end 
+    binding.pry
+    math_hash[:mean] = mean
+
+    array.each{|key| hash[key] += 1}
+    mode = hash
+    math_hash[:mode] = mode
 
 
 end 
+
+math_hash([1, 2, 3, 4, 5, 4, 6, 4, 6])
